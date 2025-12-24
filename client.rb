68040792d98313e9766ae2d56c15476bbb1102be
root <andsewal38@gmail.com>
2025-12-24 04:51:41 +0100

    almost finished client/server hello

--- client.rb
+++ client.rb
@@ -7,8 +7,11 @@ require_relative 'utils'
 require 'pry'
 require 'pry-byebug'
 
-PROTOCOL_ID = "myproto-v1"
-MSG_CLIENT_HELLO = "\x01"
+PROTOCOL_NAME = "myproto-v1"
+MSG_CLIENT_HELLO_ID = "\x01"
+MSG_SERVER_HELLO_ID = "\x02"
+
+class ProtocolError < StandardError; end
 
 class SecureClient
 
@@ -21,14 +24,95 @@ class SecureClient
     @client_pk = @client_sk.verify_key
   end
 
-  def send_message(msg)
+  # helper function used to read exactly the required size
+  def read_exact(buf, offset, len)
+  chunk = buf[offset, len]
+    if chunk.nil? || chunk.bytesize != len
+      raise ProtocolError, "Truncated #{field_name}"
+    end
+  chunk
+  end
+
+  # verify server identity and connection genuinity
+  def server_verification(opening_nonce, payload)
+    offset = 0
+    # protocl name length (2 bytes)
+    proto_len = read_exact(payload, offset, 2).unpack1("n")
+    offset += 2
+
+    # protocol name
+    proto = read_exact(payload, offset, proto_len)
+    offset += proto_len
+    raise "protocol mismatch" unless proto == PROTOCOL_NAME
+
+    # message ID
+    msg_id = read_exact(payload, offset, 1)
+    offset += 1
+    raise "Unexpected message type" unless msg_id == MSG_SERVER_HELLO_ID.ord
+
+    # role of the sender
+    role = read_exact(payload, offset, "server".bytesize)
+    offset += "server".bytesize
+    raise "Invalid sender role" unless role == "server"
+    
+    # server public key
+    server_pk_bytes = read_exact(payload, offset, 32)
+    offset += 32
+    # check quality of the key
+    begin
+      server_pk = RbNaCl::Signatures::Ed25519::VerifyKey.new(server_pk_bytes)
+    rescue RbNaCl::CryptoError
+      raise ProtocolError, "Invalid Server public key"
+    end
+    # !!!! TO BE ADD
+    # raise "Untrusted server key" unless server_pk_bytes == @trusted_server_pk
+
+    # server eph pk
+    server_eph_pk_bytes = read_exact(payload, offset, 32)
+    offset += 32
+    begin
+      server_eph_pk = RbNaCl::PublicKey.new(server_eph_pk_bytes)
+    rescue RbNaCl::CryptoError
+      raise ProtocolError, "Invalid server ephemeral public key"
+    end
+    
+    # server nonce
+    server_nonce = read_exact(payload, offset, RbNaCl::Box.nonce_bytes)
+    offset += RbNaCl::Box.nonce_bytes
+
+    # signature
+    signature = read_exact(payload, offset, 64)
+    offset += 64
+    raise "Trailing bytes detected" unless offset == payload.bytesize
+
+    # rebuild transcript
+    transcript = 
+      [PROTOCOL_NAME.bytesize].pack("n") +
+      [PROTOCOL_NAME] +
+      MSG_SERVER_HELLO_ID +
+      "server" +
+      server_pk_bytes +
+      opening_nonce +
+      server_nonce +
+      server_eph_pk_bytes
+
+    # signature verification
+    unless server_pk.verify(signature, transcript)
+      raise "Server signature verification failed"
+    end
+
+    {server_pk: server_pk, server_eph_pk: server_eph_pk, server_nonce: server_nonce}
+  end
+  
+  # initialize the connection with the server
+  def initialization(msg)
     
     # Ephemeral client key
     eph_sk = RbNaCl::PrivateKey.generate
     eph_pk = eph_sk.public_key
 
     # Sign ephemeral pub with host key, creates a signature
-    sig = @client_sk.sign(eph_pk.to_s)
+    # sig = @client_sk.sign(eph_pk.to_bytes)
 
     puts "created: ephemeral private key, public key and signature"
 
@@ -36,24 +120,23 @@ class SecureClient
     sock = TCPSocket.new(@host, @port)
     puts "TCP connection established"
 
-
+    # create the nonce used to validate the session
     opening_nonce = RbNaCl::Random.random_bytes(RbNaCl::Box.nonce_bytes)
-    opening_message = [PROTOCOL_ID.bytesize].pack("N") + PROTOCOL_ID + MSG_CLIENT_HELLO + opening_nonce
+    # send the nonce and start protocol
+    opening_message = [PROTOCOL_NAME.bytesize].pack("n") + 
+      PROTOCOL_NAME + 
+      MSG_CLIENT_HELLO_ID + 
+      opening_nonce
     
     # send the first nonce to the server
-    begin
-      write_all(sock, opening_message)
-      binding.pry
-    rescue IOError => e
-      log("Connection failed: #{e.message}")
-      exit
-    end    
+    write_all(sock, opening_message)
 
+    binding.pry
     # receive the signature and what's needed to verify it
     hello_back_payload = read_blob(sock)
 
     # verify server identity
-    server_verification(hello_back_payload)
+    server_verification(opening_nonce, hello_back_payload)
 
     # receive kex
     puts "receive kex"        
@@ -98,4 +181,4 @@ end
 client = SecureClient.new("127.0.0.1", 2222)
 print "Message: "
 msg = STDIN.gets.strip
-client.send_message(msg)
+client.initialization(msg)
